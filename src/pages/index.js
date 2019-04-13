71e76cb76a3af8261bf3e53ff05e830a5a80a12b
Moein Alizadeh <truemoein@gmail.com>
2019-04-13 16:43:21 +0430

    Initial commit.

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <h3>We're getting ready to launch soon...</h3>
+)
+
+export default IndexPage
